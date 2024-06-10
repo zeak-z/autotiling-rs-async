d8f827aee3bb3af7ff20d5ca39f86537bb8546e5
zeak-z <153205102+zeak-z@users.noreply.github.com>
2024-06-09 17:03:46 -0700

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use futures::stream::{TryStreamExt};
+use futures::stream::TryStreamExt;
 use swayipc_async::{Connection, Event, EventType, NodeLayout, WindowChange, Node};
 use tokio::sync::mpsc;
 use tokio::task;
 use std::sync::Arc;
-
 use clap::Parser;
 
 /// Switches the layout of the focused window based on its dimensions.
@@ -11,14 +10,12 @@ async fn switch_splitting(conn: &mut Connection, allowed_workspaces: &Arc<Vec<i3
     // Skip if the focused workspace is not in the allowed list (or allow all if the list is empty)
     if !allowed_workspaces.is_empty() {
         let workspaces = conn.get_workspaces().await?;
-        let focused_workspace = workspaces
-            .iter()
-            .filter_map(|w| if w.focused { Some(w.num) } else { None })
-            .next()
-            .ok_or("Could not find the focused workspace")?;
-
-        if !allowed_workspaces.contains(&focused_workspace) {
-            return Ok(());
+        if let Some(focused_workspace) = workspaces.iter().find(|w| w.focused).map(|w| w.num) {
+            if !allowed_workspaces.contains(&focused_workspace) {
+                return Ok(());
+            }
+        } else {
+            return Err("Could not find the focused workspace".into());
         }
     }
 
@@ -26,7 +23,10 @@ async fn switch_splitting(conn: &mut Connection, allowed_workspaces: &Arc<Vec<i3
     let (focused_node, parent) = find_nodes(&tree)?;
 
     // Skip if the focused node is floating, fullscreen, stacked, or tabbed
-    if let (swayipc_async::NodeType::FloatingCon, _, NodeLayout::Stacked, NodeLayout::Tabbed) = (focused_node.node_type, focused_node.percent.unwrap_or(1.0) > 1.0, focused_node.layout, focused_node.layout) {
+    if matches!(focused_node.node_type, swayipc_async::NodeType::FloatingCon)
+        || focused_node.percent.unwrap_or(1.0) > 1.0
+        || matches!(focused_node.layout, NodeLayout::Stacked | NodeLayout::Tabbed)
+    {
         return Ok(());
     }
 
@@ -52,9 +52,9 @@ async fn switch_splitting(conn: &mut Connection, allowed_workspaces: &Arc<Vec<i3
 }
 
 fn find_nodes<'a>(node: &'a Node) -> Result<(&'a Node, &'a Node), Box<dyn std::error::Error + Send + Sync>> {
-    let focused_node = node.find_focused_as_ref(&|n: &Node| n.focused)
+    let focused_node = node.find_focused_as_ref(|n: &Node| n.focused)
         .ok_or("Could not find the focused node")?;
-    let parent_node = node.find_focused_as_ref(&|n: &Node| n.nodes.iter().any(|n| n.focused))
+    let parent_node = node.find_focused_as_ref(|n: &Node| n.nodes.iter().any(|n| n.focused))
         .ok_or("Could not find the parent node")?;
     Ok((focused_node, parent_node))
 }
@@ -83,28 +83,31 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let workspaces = Arc::new(args.workspace);
 
-    task::spawn(async move {
-        let conn = Connection::new().await?;
-        let event_stream = conn.subscribe(&[EventType::Window]).await?;
+    task::spawn({
+        let workspaces = Arc::clone(&workspaces);
+        async move {
+            let mut conn = Connection::new().await?;
+            let mut event_stream = conn.subscribe(&[EventType::Window]).await?;
 
-        event_stream.try_for_each_concurrent(None, |event| {
-            if let Event::Window(window_event) = event {
-                if let WindowChange::Focus = window_event.change {
-                    let sender = sender.clone();
-                    let workspaces_clone = Arc::clone(&workspaces);
-                    task::spawn(async move {
-                        if let Err(e) = create_conn_and_switch_splitting(sender, workspaces_clone).await {
-                            eprintln!("Error occurred: {:?}", e);
-                        }
-                    });
+            event_stream.try_for_each_concurrent(None, |event| {
+                if let Event::Window(window_event) = event {
+                    if let WindowChange::Focus = window_event.change {
+                        let sender = sender.clone();
+                        let workspaces_clone = Arc::clone(&workspaces);
+                        task::spawn(async move {
+                            if let Err(e) = create_conn_and_switch_splitting(sender, workspaces_clone).await {
+                                eprintln!("Error occurred: {:?}", e);
+                            }
+                        });
+                    }
                 }
-            }
-            futures::future::ok(())
-        }).await?;
-        Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+                futures::future::ok(())
+            }).await?;
+            Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
+        }
     });
 
-    while let Some(_) = receiver.recv().await {}
+    while receiver.recv().await.is_some() {}
 
     Ok(())
 }
